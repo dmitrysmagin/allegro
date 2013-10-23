941d7a5974bd586c558d439e3903b3fe743e47d7
Dmitry Smagin <exmortis@yandex.ru>
2013-10-23 14:13:17 +0400

    Revert "Add ability to build loadpng as DLL."
    This reverts commit fed67f58ff1a0e744ae725000ba0da82426e6ab3.

--- addons/loadpng/loadpng.h
+++ addons/loadpng/loadpng.h
@@ -7,25 +7,6 @@ extern "C" {
 #endif
 
 
-#if (defined LOADPNG_DYNAMIC) && (defined ALLEGRO_WINDOWS)
-    #ifdef LOADPNG_SRC_BUILD
-        #define _APNG_DLL __declspec(dllexport)
-    #else
-        #define _APNG_DLL __declspec(dllimport)
-    #endif /* ALLEGRO_GL_SRC_BUILD */
-#else
-    #define _APNG_DLL
-#endif /* (defined LOADPNG_DYNAMIC) && (defined ALLEGRO_WINDOWS) */
-
-#define APNG_VAR(type, name) extern _APNG_DLL type name
-
-#if (defined LOADPNG_DYNAMIC) && (defined ALLEGRO_WINDOWS)
-    #define APNG_FUNC(type, name, args) extern _APNG_DLL type __cdecl name args
-#else
-    #define APNG_FUNC(type, name, args) extern type name args
-#endif /* (defined LOADPNG_DYNAMIC) && (defined ALLEGRO_WINDOWS) */
-
-
 
 /* Overkill :-) */
 #define LOADPNG_VERSION		1
@@ -45,37 +26,37 @@ extern "C" {
  *
  * Otherwise, the value of _png_screen_gamma is taken as-is.
  */
-APNG_VAR(double, _png_screen_gamma);
+extern double _png_screen_gamma;
 
 
 /* Choose zlib compression level for saving file.
  * Default is Z_BEST_COMPRESSION.
  */
-APNG_VAR(int, _png_compression_level);
+extern int _png_compression_level;
 
 
 /* Load a PNG from disk. */
-APNG_FUNC(BITMAP *, load_png, (AL_CONST char *filename, RGB *pal));
+extern BITMAP *load_png(AL_CONST char *filename, RGB *pal);
 
 /* Load a PNG from some place. */
-APNG_FUNC(BITMAP *, load_png_pf, (PACKFILE *fp, RGB *pal));
+extern BITMAP *load_png_pf(PACKFILE *fp, RGB *pal);
 
 /* Load a PNG from memory. */
-APNG_FUNC(BITMAP *, load_memory_png, (AL_CONST void *buffer, int buffer_size, RGB *pal));
+extern BITMAP *load_memory_png(AL_CONST void *buffer, int buffer_size, RGB *pal);
 
 /* Save a bitmap to disk in PNG format. */
-APNG_FUNC(int, save_png, (AL_CONST char *filename, BITMAP *bmp, AL_CONST RGB *pal));
+extern int save_png(AL_CONST char *filename, BITMAP *bmp, AL_CONST RGB *pal);
 
 /* Adds `PNG' to Allegro's internal file type table.
  * You can then just use load_bitmap and save_bitmap as usual.
  */
-APNG_FUNC(void, register_png_file_type, (void));
+extern void register_png_file_type(void);
 
 /* Register an datafile type ID with Allegro, so that when an object
  * with that type ID is encountered while loading a datafile, that
  * object will be loaded as a PNG file.
  */
-APNG_FUNC(void, register_png_datafile_object, (int id));
+extern void register_png_datafile_object(int id);
 
 /* This is supposed to resemble jpgalleg_init in JPGalleg 2.0, just in
  * case you are lazier than lazy.  It contains these 3 lines of code:
@@ -83,7 +64,7 @@ APNG_FUNC(void, register_png_datafile_object, (int id));
  *  register_png_file_type();
  *  return 0;
  */
-APNG_FUNC(int, loadpng_init, (void));
+extern int loadpng_init(void);
 
 
 #ifdef __cplusplus
